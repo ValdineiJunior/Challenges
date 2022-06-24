fc162848cf14308daf3ef57faf51e390be21165c
Valdinei de Paula Junior <43623234+ValdineiJunior@users.noreply.github.com>
2022-06-24 20:36:56 -0300

    Update plusMinus.js

--- plusMinus.js
+++ plusMinus.js
@@ -25,6 +25,8 @@ function plusMinus(arr) {
 const arr = [-4, 3, -9, 0, 4, 1]
 plusMinus(arr)
 
+// https://www.hackerrank.com/challenges/three-month-preparation-kit-plus-minus/problem
+
 // Given an array of integers, calculate the ratios of its elements that are positive, negative, and zero. Print the decimal value of each fraction on a new line with  places after the decimal.
 
 // Note: This challenge introduces precision problems. The test cases are scaled to six decimal places, though answers with absolute error of up to  are acceptable.
